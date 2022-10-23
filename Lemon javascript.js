cba90cff0b9b0ae4d08db880a647bffd3ded7e43
IncLemon <116500256+IncLemon@users.noreply.github.com>
2022-10-23 10:49:36 -0400

    Add files via upload

---
+++ Lemon javascript.js
@@ -0,0 +1,6 @@
+
+// in This form of product this will not be produced publicly only privatly
+// this form of Lemon also has NO server access or Access to our Network
+// This is also a inactive version or display version
+
+console.log("this is a very very early display model of Lemon")
